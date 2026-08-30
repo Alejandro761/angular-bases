e352ac4caaa4f00dd538ff8ae74facb07dfbf241
Alejandro Elias Castaneda Ibarra <alejandroxd62@gmail.com>
2026-08-30 15:23:40 -0600

    señales

--- src/app/pages/counter/counter-page.ts
+++ src/app/pages/counter/counter-page.ts
@@ -8,14 +8,21 @@ import { Component, signal } from "@angular/core";
 export class CounterPage {
 
   counter = 10;
+  counterSignal = signal(10);
 
   increase() {
+
     this.counter++;
+    this.counterSignal.update(current => ++current);
   }
+
   decrease() {
     this.counter--;
+    this.counterSignal.update(current => --current);
   }
+
   reset() {
     this.counter = 10;
+    this.counterSignal.set(0);
   }
 }
