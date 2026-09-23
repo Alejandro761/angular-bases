d32e9bab9de688a8cbee6e75abbdf628aca8d648
Alejandro Elias Castaneda Ibarra <alejandroxd62@gmail.com>
2026-09-22 19:42:19 -0600

    senales computadas

--- src/app/pages/hero/hero-page.ts
+++ src/app/pages/hero/hero-page.ts
@@ -1,12 +1,22 @@
-import { Component, signal } from "@angular/core";
+import { UpperCasePipe } from "@angular/common";
+import { Component, computed, signal } from "@angular/core";
 
 @Component({
-  templateUrl: 'hero-page.html'
+  templateUrl: 'hero-page.html',
+  imports: [UpperCasePipe]
 })
 
 export class HeroPage {
   name = signal('Ironman')
   age = signal(45)
+
+  // señales computadas (computed) son de solo lectura
+  heroDescription = computed(() => {
+    const description = `${this.name()} - ${this.age()}`
+    return description
+  })
+
+  capitalizedName = computed(() => this.name().toUpperCase())
 
   getHeroDescription() {
     return `${ this.name() } - ${ this.age() }`;
